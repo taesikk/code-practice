5ab3451096c934d2356c3b26fe9f4af40078ff09
taesikk <dlwhsktm@naver.com>
2025-04-01 00:03:08 +0900

    P120812 - 임시 완성본 코드 정리 필요

--- src/main/java/org/basic/P120812.java
+++ src/main/java/org/basic/P120812.java
@@ -1,35 +1,43 @@
 package org.basic;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class P120812 {
 	public void p120812() {
-		int[] array = new int[]{1,2,3,3,3,4};
-		Map<Integer, Integer> result = func(array);
+		int[] array = new int[]{1,2,3,3,3,4,4,4};
+		List<Integer> result = func(array);
 
 		System.out.println(result);
 	}
 
-	public Map<Integer, Integer> func(int[] array) {
+	public List<Integer> func(int[] array) {
 		int length = array.length;
-		int[] result = new int[length];
-		Map<Integer, Integer> result2 = new HashMap<>();
+		Arrays.sort(array); // sort
+		HashMap<Integer, Integer> temp = new HashMap<>();
+		List<Integer> result = new ArrayList<>();
 
+		int maxCnt = 0;
 		for (int i = 0; i < length; i++) {
-
 			int idxCnt = 0;
 			for (int j = 0; j < length; j++) {
 				if (array[i] == array[j]) {
 					idxCnt ++;
 				}
 			}
-			if (!result2.containsKey(array[i])) {
-				result2.put(array[i], idxCnt);
+			if (idxCnt > maxCnt) {
+				maxCnt = idxCnt;
 			}
-			result[i] = idxCnt;
+			if (!temp.containsKey(array[i])) {
+				temp.put(array[i], idxCnt);
+			}
 		}
 
-		return result2;
+		for (Integer k : temp.keySet()) {
+			if (temp.get(k) == maxCnt) {
+				result.add(k);
+			}
+		}
+
+		return result;
 	}
 }
