1597e9629539f717ee66f6f553bb46c5d6a92096
dlwhsktm <dlwhsktm@forcs.com>
2025-03-31 17:26:10 +0900

    P120812 - Map 추가 후 로직 수정

--- src/main/java/org/basic/P120812.java
+++ src/main/java/org/basic/P120812.java
@@ -1,28 +1,35 @@
 package org.basic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P120812 {
 	public void p120812() {
 		int[] array = new int[]{1,2,3,3,3,4};
-		int[] result = func(array);
-		for (int a : result) {
-			System.out.println(a);
-		}
+		Map<Integer, Integer> result = func(array);
+
+		System.out.println(result);
 	}
 
-	public int[] func(int[] array) {
+	public Map<Integer, Integer> func(int[] array) {
 		int length = array.length;
 		int[] result = new int[length];
+		Map<Integer, Integer> result2 = new HashMap<>();
 
 		for (int i = 0; i < length; i++) {
+
 			int idxCnt = 0;
 			for (int j = 0; j < length; j++) {
 				if (array[i] == array[j]) {
 					idxCnt ++;
 				}
 			}
+			if (!result2.containsKey(array[i])) {
+				result2.put(array[i], idxCnt);
+			}
 			result[i] = idxCnt;
 		}
 
-		return result;
+		return result2;
 	}
 }
