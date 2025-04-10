2ef7574342796827797b9552ee14835ba1decf9d
dlwhsktm <dlwhsktm@forcs.com>
2025-04-10 09:46:13 +0900

    P1845 - 완료

--- src/main/java/org/level1/P1845.java
+++ src/main/java/org/level1/P1845.java
@@ -1,6 +1,8 @@
 package org.level1;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class P1845 {
 	/**
 	 * 당신은 폰켓몬을 잡기 위한 오랜 여행 끝에, 홍 박사님의 연구실에 도착했습니다. 홍 박사님은 당신에게 자신의 연구실에 있는 총 N 마리의 폰켓몬 중에서 N/2마리를 가져가도 좋다고 했습니다.
@@ -14,11 +16,23 @@ public class P1845 {
 	 * 세 번째(2번), 네 번째(3번) 폰켓몬을 선택
 	 * 이때, 첫 번째(3번) 폰켓몬과 네 번째(3번) 폰켓몬을 선택하는 방법은 한 종류(3번 폰켓몬 두 마리)의 폰켓몬만 가질 수 있지만, 다른 방법들은 모두 두 종류의 폰켓몬을 가질 수 있습니다. 따라서 위 예시에서 가질 수 있는 폰켓몬 종류 수의 최댓값은 2가 됩니다.
 	 * 당신은 최대한 다양한 종류의 폰켓몬을 가지길 원하기 때문에, 최대한 많은 종류의 폰켓몬을 포함해서 N/2마리를 선택하려 합니다. N마리 폰켓몬의 종류 번호가 담긴 배열 nums가 매개변수로 주어질 때, N/2마리의 폰켓몬을 선택하는 방법 중, 가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아, 그때의 폰켓몬 종류 번호의 개수를 return 하도록 solution 함수를 완성해주세요.
+	 *
+	 * 6마리의 폰켓몬이 있으므로, 3마리의 폰켓몬을 골라야 합니다.
+	 * 가장 많은 종류의 폰켓몬을 고르기 위해서는 3번 폰켓몬 한 마리, 2번 폰켓몬 한 마리, 4번 폰켓몬 한 마리를 고르면 되며, 따라서 3을 return 합니다.
 	 */
 	public void p1845() {
+		// maxChoice와 같거나 작은 최대 폰켓몬 마리 수
 		int[] nums = new int[]{3,3,3,2,2,4};
 		int maxChoice = nums.length / 2;
 
+		List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		numList = numList.stream().distinct().collect(Collectors.toList());
+
+		if (numList.size() > maxChoice) {
+			System.out.println(maxChoice);
+		} else {
+			System.out.println(numList.size());
+		}
 
 	}
 }
